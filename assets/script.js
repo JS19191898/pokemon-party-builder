35de87f206ab0ad400d03efc3d3fb4899e63060f
Novia Phan <noviaphan@gmail.com>
2022-04-09 14:32:36 -0400

    CSS: added media query, HTML: options, SCRIPT: comments

---
+++ assets/script.js
@@ -0,0 +1,16 @@
+// global Variables
+var searchTextEl = document.querySelector("#searchbar");
+
+//fetch function
+
+//function that takes user input for search citeria
+
+//function that gets search results based on user search criteria
+
+//function to save user input to local storage
+
+//function that displays selected pokémon
+
+//function that suggests what other types of pokemon would compliment the user's selection thus far
+
+//function that lets user take a photo of their entire selected party
