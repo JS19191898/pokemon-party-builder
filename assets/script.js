97ff2d6f873130ce7fe2ad5df1df5dd87421bb01
Jonathan Swann <jonathanswann87@gmail.com>
2022-04-14 22:19:50 -0400

    added card elements, added js inner html, fetch api, etc.

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,92 @@
 // global Variables
 var searchTextEl = document.querySelector("#searchbar");
+var generationSelectEl = document.querySelector("#select-generation");
+var typeSelectEl = document.querySelector("#select-type");
+var strengthSelectEl = document.querySelector("#select-strength");
+var textUserInputEl = document.querySelector("#pokemon");
+var baseUrl = "https://pokeapi.co/api/v2/";
+var searchFormEl = document.querySelector("#search-form");
+var containerEl = document.querySelector("#container");
 
-//fetch function
+//functions that perform search citeria ------------------------->
+//Generates pokemon result from the input in the Search bar
+function generatePokemon(pokemon) {
+  var baseUrl = `https://pokeapi.co/api/v2/pokemon/${pokemon}/`;
+  console.log(pokemon);
+  fetch(baseUrl)
+    .then((response) => {
+      console.log(response);
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+      generateCard(data);
+    });
+}
+//generates pokemon results from "Generation" input
 
-//function that takes user input for search citeria
+//generates pokemon results from "type" input
 
-//function that gets search results based on user search criteria
+//generates pokemon results from "effective against" input
 
-//function to save user input to local storage
+//TODO: function that saves pokemon (into local storage) after user selects it -----------
+//(this will need to make the container results clickable with an eventlistener, and saving to the local storage)
 
-//function that displays selected pokémon
+// ad.eventlistener to 'click' submit button
 
-//function that suggests what other types of pokemon would compliment the user's selection thus far
+// API from PokeAPI var baseUrl https://pokeapi.co/api/v2/type/{id or name}/
 
-//function that lets user take a photo of their entire selected party
+// grabs parent div to generate card from given types of search options
+function generateCard(data) {
+  var div = document.createElement("div");
+  div.class = "tile is-ancestor";
+  div.innerHTML = ` <div class="tile is-parent">
+
+  <div class="card coulmn tile is-child is-3">
+      <div class="card-image">
+          <figure class="image is-4by3">
+              <img src="https://www.serebii.net/swordshield/pokemon/001.png" alt="Bulbasaur">
+          </figure>
+      </div>
+      <div class="card-content">
+          <div class="media">
+              <div class="media-left">
+                  <p class="title is-3">Bulbasaur</p>
+                  <p class="subtitle is-5">The Seed Pokemon</p>
+              </div>
+              <div class="media-content ">
+
+                  <div class="type-image title is-pulled-right is-4">
+                      <p class="type grass">Grass</p>
+                      <p class="type poison">Poison</p>
+                  </div>    
+                  <div class="subtitle is-pulled-right">
+                  </div>
+              </div>
+          </div>
+          <div class="content">
+              For some time after its birth, it grows by taking nourishment from the seed on its back.
+              <p> type:${data.type}
+              </p>
+          </div>
+
+          
+      </div>
+  </div>
+  </div>`;
+
+  // clears out search
+  containerEl.innerHTML = "";
+
+  containerEl.appendChild(div);
+}
+// generatePokemon();
+
+searchFormEl.addEventListener("submit", function (event) {
+  event.preventDefault();
+  // console.log("clicky click")
+  generatePokemon(textUserInputEl.value);
+  // console.log(textUserInputEl.value);
+});
+
+//function that lets user take a photo of their entire selected party (OPTIONAL)
