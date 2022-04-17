c1902414b41f8031b18f5262ef36765dd1504925
Jonathan Swann <jonathanswann87@gmail.com>
2022-04-17 17:29:15 -0400

    Added js function for filter by generation

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,6 @@
 // global Variables
 var searchTextEl = document.querySelector("#searchbar");
-var generationSelectEl = document.querySelector("#select-generation");
+var generationSelectEl = document.querySelector("#generation-select");
 var typeSelectEl = document.querySelector("#select-type");
 var strengthSelectEl = document.querySelector("#select-strength");
 var textUserInputEl = document.querySelector("#pokemon");
@@ -21,7 +21,9 @@ function generatePokemon(pokemon) {
     .then((response) => {
       console.log(response.status);
       if (response.status === 404) {
+        console.log(response.status);
         modalAlert();
+        return null;
       }
 
       return response.json();
@@ -29,6 +31,35 @@ function generatePokemon(pokemon) {
     .then((data) => {
       console.log(data);
       generateCard(data);
+    });
+}
+
+function findGeneration(generation) {
+  //debugger
+
+  var baseUrl = `https://pokeapi.co/api/v2/generation/${generation}/`;
+  console.log(generation);
+
+  fetch(baseUrl)
+    .then((response) => {
+      console.log(response);
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data.pokemon_species);
+
+      data.pokemon_species.forEach((element) => {
+        console.log(element.name);
+        generatePokemon(element.name);
+      });
+
+      // for(var i = 0; i < data.pokemon_species.length; i++) {
+      //   console.log(data.pokemon_species[i].name);
+      //   generatePokemon(data.pokemon_species[i].name);
+      // }
+
+      // console.log(data.pokemon_species[0].name);
+      // generatePokemon(data.pokemon_species[0].name);
     });
 }
 
@@ -48,9 +79,16 @@ function closeModal() {
 //TODO: function that saves pokemon (into local storage) after user selects it -----------
 //(this will need to make the container results clickable with an eventlistener, and saving to the local storage)
 
+// ad.eventlistener to 'click' submit button
+
+// API from PokeAPI var baseUrl https://pokeapi.co/api/v2/type/{id or name}/
 
 // grabs parent div to generate card from given types of search options
 function generateCard(data) {
+
+  // clears out previous results
+  containerEl.innerHTML = "";
+
   //Fetch call to get the pokedex description
   var baseUrl = `https://pokeapi.co/api/v2/pokemon-species/${data.name}/`;
   console.log(data.name);
@@ -105,10 +143,11 @@ function generateCard(data) {
       </div>
   </div>
   </div>
-  <button  class="button is-primary">Add to Party!</button>`;
+  <button  class="button is-primary">Add to Party!</button>
+  <button  class="button is-primary">Remove from Party!</button>
+  <button  class="button is-primary">Clear arty!</button>`;
 
-      // clears out search
-      containerEl.innerHTML = "";
+
 
       containerEl.appendChild(div);
     });
@@ -122,6 +161,17 @@ searchFormEl.addEventListener("submit", function (event) {
   // console.log(textUserInputEl.value);
 });
 
+generationSelectEl.addEventListener("click", function (event) {
+  //debugger
+
+  event.preventDefault();
+  //console.log(event.target.value);
+
+  findGeneration(event.target.value);
+});
+
+generationSelectEl.addEventListener("change", function (event) {});
+
 closeModalEl.addEventListener("click", closeModal);
 
 //function that lets user take a photo of their entire selected party (OPTIONAL)
@@ -129,9 +179,6 @@ closeModalEl.addEventListener("click", closeModal);
 // !<-------------------------------->
 // // Saving to local storage
 // localStorage.setItem():
-
-// make an array 
-// save data
 
 // Remove Data from Local Storage
 // localStorage.removeItem(key)
